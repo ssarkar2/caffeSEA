31ccec35000b2e265ebe0b880f90db7c472dabbd
akumar14 <akumar14@umd.edu>
2016-04-07 19:56:22 -0400

    added training with python

--- train/trainModel.py
+++ train/trainModel.py
@@ -1,9 +1,13 @@
 from trainconfig import *
 from utils.loadDataUtils import convertMatToHDF5
+import caffe
 
 def trainToySEA():
     cfg = getConfig()
     convertMatToHDF5(cfg.toyTrainData, cfg.hdf5ToyDir, 1)  #set readMode to 0, in case file already exists for faster execution
+	caffe.set_mode_gpu()
+	solver = caffe.get_solver("solver.prototxt")
+	solver.solve()
     print 'hello'
         
 def trainFullSEA():
