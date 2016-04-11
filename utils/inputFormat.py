cabbd78d7cdb8bdcef292c5a878131eb119e9175
Rishabh Bindal <rishabhbindal7@users.noreply.github.com>
2016-04-10 20:30:08 -0400

    Create inputFormat.py
    function to convert input Fasta file to HDF5 format

---
+++ utils/inputFormat.py
@@ -0,0 +1,12 @@
+from subprocess import *
+
+def fasta2hdf5(input_file, output_directory):
+    tempdir = output_directory
+    check_call(['cp', input_file, tempdir + '/input_file.fasta'])
+    print "Successfully copied input to working directory."
+    check_call(["grep '>'  " + tempdir + '/input_file.fasta ' + ">" + tempdir + '/input_file.fasta.name'], shell=True)
+    try:
+        check_call(["python 1_fasta2input.nomut.py  " + tempdir + "/input_file.fasta"], shell=True)
+    except:
+        raise Exception("Fasta format error.")
+    print "Successfully converted to input format"
